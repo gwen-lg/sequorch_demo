3d2fc5a8e867137e763ce62a25d0d27e25f90b55
Gwen Lg <me@gwenlg.fr>
2023-06-08 22:07:15 +0200

    add bevy dependency and use it to create a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use bevy::{prelude::*, window::PresentMode};
+
 fn main() {
-	println!("Hello, world!");
+	App::new()
+		.add_plugins(DefaultPlugins.set(WindowPlugin {
+			primary_window: Some(Window {
+				title: "SequOrch Demo".to_string(),
+				resolution: (1280.0, 768.0).into(),
+				present_mode: PresentMode::Immediate,
+				..Default::default()
+			}),
+			..Default::default()
+		}))
+		.run();
 }
