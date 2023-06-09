e72fa30adc112e6b01a65a0bc9e83146e58d298f
Gwen Lg <me@gwenlg.fr>
2023-06-09 16:08:51 +0200

    map: add random map generation

--- src/game/map.rs
+++ src/game/map.rs
@@ -1,5 +1,6 @@
 use bevy::ecs::system::EntityCommands;
 use bevy::prelude::*;
+use rand::{thread_rng, Rng};
 
 const LEVEL_SIZE: usize = 150;
 
@@ -13,15 +14,34 @@ pub fn spawn_floor(mut commands: Commands) {
 fn create_world(width: usize) -> Vec<usize> {
 	let mut heights: Vec<usize> = Vec::with_capacity(width);
 	let mut height = 1;
-	(0..width).for_each(|x| {
+	(0..width).for_each(|_| {
 		heights.push(height);
-		height = get_height(x)
+		height = get_next_height(height)
 	});
 	heights
 }
 
-fn get_height(x: usize) -> usize {
-	1 + ((x / 5) % 3)
+fn get_next_height(current_height: usize) -> usize {
+	const MAX_MAP_HEIGHT: usize = 5;
+	let next_height = current_height as isize + get_random_height_delta();
+	return if next_height > 0 {
+		std::cmp::min(next_height as usize, MAX_MAP_HEIGHT)
+	} else {
+		1
+	};
+}
+fn get_random_height_delta() -> isize {
+	let mut rng = thread_rng();
+	let random_number: u32 = rng.gen_range(0..100);
+	let delta = match random_number {
+		0..=70 => 0,
+		71..=82 => -1,
+		83..=94 => 1,
+		95..=96 => 2,
+		97..=98 => -2,
+		_ => 0,
+	};
+	delta
 }
 
 fn add_sprites(commands: &mut EntityCommands, world: &Vec<usize>) {
