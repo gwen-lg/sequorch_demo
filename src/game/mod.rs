e78bff7f4e7437275806179c6d535fc5bdbc3a46
Gwen Lg <me@gwenlg.fr>
2023-06-13 21:24:56 +0200

    vrac & tests

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,11 +1,12 @@
 use bevy::prelude::*;
-use bevy::render::camera::ScalingMode;
+use bevy::sprite::Anchor;
 
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
 	fn build(&self, app: &mut App) {
-		app.add_startup_system(setup_camera);
+		app.add_startup_system(setup_camera)
+			.add_startup_system(setup_test_map);
 	}
 }
 
@@ -14,10 +15,45 @@ pub fn setup_camera(mut commands: Commands) {
 	camera.projection = OrthographicProjection {
 		far: 1000.0,
 		//depth_calculation: DepthCalculation::ZDifference,
-		scaling_mode: ScalingMode::FixedHorizontal(5.),
+		//scaling_mode: ScalingMode::FixedHorizontal(5.),
+		scale: 0.1,
 		..Default::default()
 	};
-	camera.transform.scale = Vec3::new(10., 10., 1.);
+	camera.transform.translation = Vec3::new(128. / 2., 76.8 / 2., 999.0);
+	// 1280.0, 768.0
 
 	commands.spawn(camera);
+}
+
+pub fn setup_test_map(mut commands: Commands) {
+	commands.spawn(SpriteBundle {
+		sprite: Sprite {
+			custom_size: Some(Vec2::new(1., 75.8)),
+			color: Color::rgb(0.9, 0.2, 0.9),
+			anchor: Anchor::BottomLeft,
+			..Default::default()
+		},
+		transform: Transform::from_xyz(0. as f32, 1. as f32 / 2., 0.),
+		..Default::default()
+	});
+	commands.spawn(SpriteBundle {
+		sprite: Sprite {
+			custom_size: Some(Vec2::new(1., 75.8)),
+			color: Color::rgb(0.9, 0.2, 0.9),
+			anchor: Anchor::BottomLeft,
+			..Default::default()
+		},
+		transform: Transform::from_xyz(127. as f32, 1. as f32, 0.),
+		..Default::default()
+	});
+	commands.spawn(SpriteBundle {
+		sprite: Sprite {
+			custom_size: Some(Vec2::new(128., 1.)),
+			color: Color::rgb(0.2, 0.9, 0.9),
+			anchor: Anchor::BottomLeft,
+			..Default::default()
+		},
+		transform: Transform::from_xyz(0. as f32, 0. as f32, 0.),
+		..Default::default()
+	});
 }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ use bevy::prelude::*;
 use bevy::window::PresentMode;
 use bevy_editor_pls::prelude::*;
 
+#[derive(Debug, Default, Clone, Eq, PartialEq, Hash, States)]
+enum WorldState {
+	#[default]
+	Run,
+	Save,
+}
+
 fn main() {
 	App::new()
 		.add_plugins(DefaultPlugins.set(WindowPlugin {
@@ -17,7 +24,47 @@ fn main() {
 			}),
 			..Default::default()
 		}))
+		.add_state::<WorldState>()
+		//.add_system(save_world.in_shedule(OnEnter(WorldState::Save)))
+		.add_system(exit_save_state.in_set(OnUpdate(WorldState::Save)))
 		.add_plugin(EditorPlugin::default())
 		.add_plugin(GamePlugin)
+		.add_system(keyboard_input_system)
+		.add_system(save_world)
 		.run();
+}
+
+fn keyboard_input_system(
+	keyboard_input: Res<Input<KeyCode>>,
+	mut app_state: ResMut<NextState<WorldState>>,
+) {
+	if keyboard_input.pressed(KeyCode::S) {
+		app_state.set(WorldState::Save);
+		//TODO change state
+	}
+}
+
+fn save_world(_world: &mut World) {
+	// let mut query = world.query_filtered::<Entity, Without<NotInScene>>();
+	// let entities: Vec<_> = query.iter(world).collect();
+
+	// let type_registry = world.get_resource::<AppTypeRegistry>().unwrap();
+	// let mut scene_builder =
+	// 	DynamicSceneBuilder::from_world_with_type_registry(world, type_registry.clone());
+	// scene_builder.extract_entities(entities.into_iter());
+	// let scene = scene_builder.build();
+
+	// let ron = scene.serialize_ron(type_registry);
+	// match ron {
+	// 	Ok(ron) => {
+	// 		println!("Scene : {ron}");
+	// 	}
+	// 	Err(err) => {
+	// 		println!("Error : {err}");
+	// 	}
+	// }
+}
+
+fn exit_save_state(mut app_state: ResMut<NextState<WorldState>>) {
+	app_state.set(WorldState::Run);
 }
