d95d2429f8d530a7d50f44c342ad9bde4159737e
Gwen Lg <me@gwenlg.fr>
2023-06-14 18:00:09 +0200

    game: add door entities

---
+++ src/game/door.rs
@@ -0,0 +1,24 @@
+use bevy::prelude::*;
+use bevy::sprite::Anchor;
+
+const DOOR_COLOR: Color = Color::rgb(0.2, 0.2, 0.9);
+
+#[derive(Component)]
+struct Door;
+
+pub fn spawn_door(commands: &mut Commands, size: Vec2, transform: Transform) {
+	let door = (
+		SpriteBundle {
+			sprite: Sprite {
+				custom_size: Some(size),
+				color: DOOR_COLOR,
+				anchor: Anchor::BottomLeft,
+				..Default::default()
+			},
+			transform,
+			..Default::default()
+		},
+		Door,
+	);
+	commands.spawn(door);
+}
--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,3 +1,4 @@
+mod door;
 mod level;
 
 use bevy::prelude::*;
@@ -7,7 +8,8 @@ pub struct GamePlugin;
 impl Plugin for GamePlugin {
 	fn build(&self, app: &mut App) {
 		app.add_startup_system(setup_camera)
-			.add_startup_system(level::setup_test_map);
+			.add_startup_system(level::setup_test_map)
+			.add_startup_system(setup_doors);
 	}
 }
 
@@ -24,4 +26,11 @@ pub fn setup_camera(mut commands: Commands) {
 		Vec3::new(VIEWPORT_CAM_VIEW.x / 20., VIEWPORT_CAM_VIEW.y / 20., 999.0);
 
 	commands.spawn(camera);
+}
+
+pub fn setup_doors(mut commands: Commands) {
+	const DOOR_SIZE: Vec2 = Vec2::new(10., 10.);
+	door::spawn_door(&mut commands, DOOR_SIZE, Transform::from_xyz(10., 1.0, 0.));
+	door::spawn_door(&mut commands, DOOR_SIZE, Transform::from_xyz(50., 1.0, 0.));
+	door::spawn_door(&mut commands, DOOR_SIZE, Transform::from_xyz(105., 1.0, 0.));
 }
