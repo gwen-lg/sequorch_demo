0166d16a2ada55e90f82461a65f6eb8dbd78951b
Gwen Lg <me@gwenlg.fr>
2023-06-30 14:43:16 +0200

    flow: add from to create a FlowProgress with a f32

--- src/sequorch/data/flow.rs
+++ src/sequorch/data/flow.rs
@@ -17,11 +17,27 @@ pub const FLOW_TIME_CS: FlowScale = FlowScale::Time(TimeUnit(100));
 pub const FLOW_PERCENT: FlowScale = FlowScale::Subdivision(SubUnit(100));
 pub const FLOW_PERTHOUSAND: FlowScale = FlowScale::Subdivision(SubUnit(1000));
 
-#[derive(Debug, Deserialize, Clone, Copy)]
+#[derive(Debug, Deserialize, PartialEq, PartialOrd, Clone, Copy)]
 pub struct FlowProgress(u32);
 
 impl FlowProgress {
 	pub fn new(progress: u32) -> Self {
 		Self(progress)
+	}
+
+	/// TODO: improve compute management
+	pub fn from(flow_scale: FlowScale, progress: f32) -> Self {
+		match flow_scale {
+			FlowScale::Time(time) => {
+				let TimeUnit(time) = time;
+				let progress = progress * time as f32;
+				FlowProgress::new(progress as u32)
+			}
+			FlowScale::Subdivision(sub) => {
+				let SubUnit(sub) = sub;
+				let progress = progress / sub as f32;
+				FlowProgress::new(progress as u32)
+			}
+		}
 	}
 }
