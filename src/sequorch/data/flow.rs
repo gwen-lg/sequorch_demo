a41ea4c5890f1c98f109a77a68efcb2477ef5e68
Gwen Lg <me@gwenlg.fr>
2023-06-30 14:42:36 +0200

    flow: add Clone and Copy derive trait

--- src/sequorch/data/flow.rs
+++ src/sequorch/data/flow.rs
@@ -1,12 +1,12 @@
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone, Copy)]
 pub struct TimeUnit(u32);
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone, Copy)]
 pub struct SubUnit(u32);
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone, Copy)]
 pub enum FlowScale {
 	Time(TimeUnit),
 	Subdivision(SubUnit),
@@ -17,7 +17,7 @@ pub const FLOW_TIME_CS: FlowScale = FlowScale::Time(TimeUnit(100));
 pub const FLOW_PERCENT: FlowScale = FlowScale::Subdivision(SubUnit(100));
 pub const FLOW_PERTHOUSAND: FlowScale = FlowScale::Subdivision(SubUnit(1000));
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone, Copy)]
 pub struct FlowProgress(u32);
 
 impl FlowProgress {
